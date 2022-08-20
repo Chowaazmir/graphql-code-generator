e233cdc0dbeaf0c37a4aeb63387061a51dc1555a
Dimitri POSTOLOV <dmytropostolov@gmail.com>
2022-08-20 10:49:52 +0200

    set `images.unoptimized = true`

--- website/next.config.mjs
+++ website/next.config.mjs
@@ -2,7 +2,7 @@ import { withGuildDocs } from 'guild-docs/next.config';
 import { CategoryToPackages } from './src/category-to-packages.mjs';
 
 const PLUGINS_REDIRECTS = Object.entries(CategoryToPackages).flatMap(([category, packageNames]) =>
-  packageNames.map(packageName => [`/plugins/${packageName}`, `/plugins/${category}/${packageName}`])
+  packageNames.map(packageName => [`/plugins/${packageName}`, `/plugins/${category}/${packageName}`]),
 );
 
 // TODO: Fix and use the correct branch name here, instead of the experimental branch
@@ -22,9 +22,9 @@ export default withGuildDocs({
       'https://pbs.twimg.com/profile_images/1004185780313395200/ImZxrDWf_400x400.jpg',
       'https://raw.githubusercontent.com/mswjs/msw/HEAD/media/msw-logo.svg',
     ],
-  },
-  images: {
-    unoptimized: true // doesn't work with `next export`
+    images: {
+      unoptimized: true, // doesn't work with `next export`
+    }
   },
   typescript: {
     // Todo: remove it before merge to master
@@ -39,18 +39,18 @@ export default withGuildDocs({
   },
   redirects: () =>
     Object.entries({
-      '/docs/presets/:presetName': '/plugins/:presetName-preset',
-      '/docs/plugins/:pluginName': '/plugins/:pluginName',
-      '/docs/getting-started/config-reference/codegen-config': '/docs/config-reference/codegen-config',
-      '/docs/getting-started/codegen-config': '/docs/config-reference/codegen-config',
-      '/docs/getting-started/documents-field': '/docs/config-reference/documents-field',
-      '/docs/getting-started/schema-field': '/docs/config-reference/schema-field',
-      '/docs/getting-started/config-field': '/docs/config-reference/config-field',
-      '/docs/getting-started/lifecycle-hooks': '/docs/config-reference/lifecycle-hooks',
-      '/docs/getting-started/require-field': '/docs/config-reference/require-field',
-      '/docs/getting-started/naming-convention': '/docs/config-reference/naming-convention',
-      '/docs/getting-started/how-does-it-work': '/docs/advanced/how-does-it-work',
-    })
+        '/docs/presets/:presetName': '/plugins/:presetName-preset',
+        '/docs/plugins/:pluginName': '/plugins/:pluginName',
+        '/docs/getting-started/config-reference/codegen-config': '/docs/config-reference/codegen-config',
+        '/docs/getting-started/codegen-config': '/docs/config-reference/codegen-config',
+        '/docs/getting-started/documents-field': '/docs/config-reference/documents-field',
+        '/docs/getting-started/schema-field': '/docs/config-reference/schema-field',
+        '/docs/getting-started/config-field': '/docs/config-reference/config-field',
+        '/docs/getting-started/lifecycle-hooks': '/docs/config-reference/lifecycle-hooks',
+        '/docs/getting-started/require-field': '/docs/config-reference/require-field',
+        '/docs/getting-started/naming-convention': '/docs/config-reference/naming-convention',
+        '/docs/getting-started/how-does-it-work': '/docs/advanced/how-does-it-work',
+      })
       .concat(PLUGINS_REDIRECTS)
       .map(([from, to]) => ({
         source: from,
